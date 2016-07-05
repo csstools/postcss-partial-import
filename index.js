23e640821b76024c65aac3a13c76609145491ff9
Pete Cook <pete@cookpete.com>
2016-07-05 17:20:23 +0100

    Apply addDependency to nested imports

--- index.js
+++ index.js
@@ -76,6 +76,11 @@ module.exports = postcss.plugin('postcss-partial-import', function (opts) {
 
 					// Push the promise into the imports array
 					imports.push(importPromise);
+
+					// Add a dependency on the nested import
+					if (hasAddDependencyMethod) {
+						opts.addDependencyTo.addDependency(path.join(dir, link));
+					}
 				}
 			}
 		});
